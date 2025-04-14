43e5e777ef8751cf8020fc998c0519fb92826591
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2025-04-14 18:04:59 +0300

    refactoring header categories test

---
+++ goodshop_tests_suite/tests/chrome_tests/categories_data.py
@@ -0,0 +1,146 @@
+# Данные категорий и подкатегорий
+categories = {
+    "Accessories": {
+        "subcategories": [
+            "Bags",
+            "Designer Accessories",
+            "Eyewear",
+            "Luggage",
+            "Watches"
+        ],
+        "stores": [
+            "Coach",
+            "Alex and Ani",
+            "Kate Spade",
+            "Shop at Future Memories"
+        ]
+    },
+    "Art": {
+        "subcategories": [
+            "Art Supplies",
+            "Portrait Studios",
+            "Prints"
+        ],
+        "stores": [
+            "Photography.com",
+            "CanvasDiscount",
+            "Blick Art Materials",
+            "Shop at Art.com"
+        ]
+    },
+    "Automotive": {
+        "subcategories": [
+            "Auto Accessories",
+            "Auto Parts",
+            "Auto Repair",
+            "Motorcycle",
+            "Motorsports",
+            "Tires",
+            "Truck"
+        ],
+        "stores": [
+            "Safelite Auto Glass",
+            "Tire Rack",
+            "Advance Auto Parts",
+            "eManualOnline.com"
+        ]
+    },
+    "Baby": {
+        "subcategories": [
+            "Baby Care",
+            "Baby Clothing",
+            "Baby Furniture",
+            "Diapers"
+        ],
+        "stores": [
+            "Ergobaby",
+            "The Honest Company",
+            "WeeSprout",
+            "Chicco"
+        ]
+    },
+    "Beauty": {
+        "subcategories": [
+            "Bath And Body",
+            "Fragrances",
+            "Hair Care",
+            "Makeup",
+            "Nail Care",
+            "Salon",
+            "Skin Care",
+            "Spa Services"
+        ],
+        "stores": [
+            "Sephora",
+            "Aveda",
+            "MAC Cosmetics",
+            "Honest Beauty"
+        ]
+    },
+    "Books and News": {
+        "subcategories": [
+            "Books",
+            "Magazines",
+            "Newspapers"
+        ],
+        "stores": [
+            "AbeBooks",
+            "Better World Books",
+            "Indigo Books & Music",
+            "Bigger Books"
+        ]
+    },
+    "Clothing": {
+        "subcategories": [
+            "Activewear",
+            "Juniors' Clothing",
+            "Kids' Clothing",
+            "Men's Clothing",
+            "Outerwear",
+            "Novelty Shirts",
+            "Socks",
+            "Swimwear",
+            "Underwear",
+            "Women's Clothing",
+            "Workwear"
+        ],
+        "stores": [
+            "ASOS",
+            "Old Navy",
+            "Lands' End",
+            "ABLE Clothing"
+        ]
+    },
+    "Crafts": {
+        "subcategories": [
+            "Craft Supplies",
+            "Hobby",
+            "Scrapbooking",
+            "Sewing"
+        ],
+        "stores": [
+            "fabric.com",
+            "JOANN Fabric & Craft",
+            "Michaels",
+            "Hobby Lobby"
+        ]
+    },
+    "Electronics": {
+        "subcategories": [
+            "Audio",
+            "Battery",
+            "Cameras",
+            "Cell Phones",
+            "Computers",
+            "Electronic Accessories",
+            "GPS",
+            "Headphones",
+            "Phones",
+            "Printers",
+            "Television"
+        ],
+        "stores": [
+            "Monoprice"
+        ]
+    }
+}
